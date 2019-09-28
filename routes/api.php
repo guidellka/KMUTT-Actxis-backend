a3afc1e3524332e5deadec07220fa7a3a7015a56
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-09-28 19:44:56 +0700

    [CREATE] create default routes in api

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,26 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::prefix('organizations')->group(function () {
-    Route::resource('{id}/projects', 'OrganizationProjectController')->only([
-        'index'
-    ]);
-});
+Route::resource('budgets','BudgetController');
+Route::resource('user_data','UserDataController');
+Route::resource('clubs','ClubController');
+Route::resource('comments','CommentController');
+Route::resource('document_category','DocumentCategoryController');
+Route::resource('documents','DocumentController');
+Route::resource('document_file','DocumentFileController');
+Route::resource('document_step','DocumentStepController');
+Route::resource('steps','StepController');
+Route::resource('photos','PhotoController');
+Route::resource('notifications','NotificationController');
+Route::resource('organizations','OrganizationController');
+Route::resource('organization_user','OrganizationUserController');
+Route::resource('permissions','PermissionController');
+Route::resource('permission_role','PermissionRoleController');
+Route::resource('roles','RoleController');
+Route::resource('role_user','RoleUserController');
+Route::resource('users','UserController');
+Route::resource('user_data','UserDataController');
