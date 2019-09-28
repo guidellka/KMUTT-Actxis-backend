5fda599c116362ff9e9fca58e0c9f0f06d8ec415
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-09-28 19:41:21 +0700

    [CREATE] create all model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,34 +1,10 @@
 <?php
 
-/**
- * Created by Reliese Model.
- * Date: Sun, 14 Apr 2019 20:05:04 +0700.
- */
-
 namespace App\Models;
 
-use Zizaco\Entrust\EntrustRole;
+use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Role
- * 
- * @property int $id
- * @property string $name
- * @property string $display_name
- * @property string $description
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * 
- * @property \Illuminate\Database\Eloquent\Collection $permissions
- * @property \Illuminate\Database\Eloquent\Collection $users
- *
- * @package App\Models
- */
-class Role extends EntrustRole
+class Role extends Model
 {
-	protected $fillable = [
-		'name',
-		'display_name',
-		'description'
-	];
+    //
 }
