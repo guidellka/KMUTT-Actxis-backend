355f9c28b296dbde0bcd6e3e34edd4d272a45b90
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2020-01-07 22:46:27 +0700

    [DELETE] delete table roles and permissions

--- database/migrations/2014_10_12_000000_create_user_tables.php
+++ database/migrations/2014_10_12_000000_create_user_tables.php
@@ -28,13 +28,11 @@ class CreateUserTables extends Migration
         Schema::create('user_data', function (Blueprint $table) {
             $table->unsignedInteger('user_id');
             $table->string('first_name', 64);
-            $table->string('middle_name', 64)->nullable();
             $table->string('last_name', 64);
             $table->string('student_id', 11)->nullable()->unique();
             $table->string('department',80)->nullable(); 
-            $table->string('faculty', 80)->nullable(); 
             $table->string('branch', 80)->nullable(); 
-            $table->string('email');
+            $table->string('email', 100)->nullable();
             $table->string('tel_no', 16)->nullable();
             $table->datetime('created_at')->useCurrent();
             $table->datetime('updated_at')
@@ -45,59 +43,6 @@ class CreateUserTables extends Migration
             $table->foreign('user_id')->references('id')->on('users')
                 ->onUpdate('restrict')->onDelete('cascade');
         });
-
-        // Create table for storing roles
-        Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
-            $table->string('name')->unique();
-            $table->string('display_name')->nullable();
-            $table->string('description')->nullable();
-            $table->datetime('created_at')->useCurrent();
-            $table->datetime('updated_at')
-                ->default(
-                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
-                );
-        });
-
-        // Create table for associating roles to users (Many-to-Many)
-        Schema::create('role_user', function (Blueprint $table) {
-            $table->unsignedInteger('user_id');
-            $table->unsignedInteger('role_id');
-
-            $table->foreign('user_id')->references('id')->on('users')
-                ->onUpdate('restrict')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')
-                ->onUpdate('cascade')->onDelete('cascade');
-
-            $table->unique(['user_id', 'role_id']);
-        });
-
-        // Create table for storing permissions
-        Schema::create('permissions', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
-            $table->string('name')->unique();
-            $table->string('display_name')->nullable();
-            $table->string('description')->nullable();
-            $table->datetime('created_at')->useCurrent();
-            $table->datetime('updated_at')
-                ->default(
-                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
-                );
-        });
-
-        // Create table for associating permissions to roles (Many-to-Many)
-        Schema::create('permission_role', function (Blueprint $table) {
-            $table->unsignedInteger('permission_id');
-            $table->unsignedInteger('role_id');
-
-            $table->foreign('permission_id')->references('id')->on('permissions')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')
-                ->onUpdate('cascade')->onDelete('cascade');
-
-            $table->unique(['permission_id', 'role_id']);
-        });
-
 
         DB::commit();
     }
@@ -111,9 +56,5 @@ class CreateUserTables extends Migration
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('user_data');
-        Schema::dropIfExists('roles');
-        Schema::dropIfExists('role_user');
-        Schema::dropIfExists('permissions');
-        Schema::dropIfExists('permission_role');
     }
 }
