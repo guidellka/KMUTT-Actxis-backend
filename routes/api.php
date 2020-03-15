080b3286184848b91f409c03b416fd82b1b135d3
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2020-03-15 17:41:31 +0700

    [UPDATE] edit all route

--- routes/api.php
+++ routes/api.php
@@ -7,26 +7,24 @@ use Illuminate\Http\Request;
 // });
 
 Route::resource('budgets','BudgetController');
-Route::resource('user_data','UserDataController');
 Route::resource('clubs','ClubController');
 Route::resource('comments','CommentController');
-Route::resource('document_category','DocumentCategoryController');
+Route::resource('document_categories','DocumentCategoryController');
 Route::resource('documents','DocumentController');
-Route::resource('document_file','DocumentFileController');
-Route::resource('document_step','DocumentStepController');
+Route::resource('attach_files','AttachFileController');
+Route::resource('document_steps','DocumentStepController');
+Route::resource('category_steps','CategoryStepController');
 Route::resource('steps','StepController');
 Route::resource('photos','PhotoController');
 Route::resource('notifications','NotificationController');
 Route::resource('organizations','OrganizationController');
-Route::resource('organization_user','OrganizationUserController');
-Route::resource('permissions','PermissionController');
-Route::resource('permission_role','PermissionRoleController');
-Route::resource('roles','RoleController');
-Route::resource('role_user','RoleUserController');
+Route::resource('organization_users','OrganizationUserController');
 Route::resource('users','UserController');
 Route::resource('user_data','UserDataController');
 
 Route::get('user/{username}', 'UserController@getByUsername');
 Route::get('organ_user/{user_id}', 'OrganizationUserController@getWithName');
+Route::get('documentByOwnerId/{owner_id}', 'DocumentController@getDocumentListsByOwnerId');
+Route::get('/getLecturer', 'UserDataController@getLecturer');
 
 
